d739169191331c2639c6634eecf2ea857707e406
Harikrishnan <hkkrishn563@gmail.com>
2020-09-22 17:58:12 -0700

    JS fundamentals

---
+++ BSTs/ClosestValBST.js
@@ -0,0 +1,8 @@
+//Write a function that takes in a Binary Search Tree and a target interger value and returns the closest value to that target value in the BST
+//YOu can assuem that there will only be one Closest Value
+//Each BST node has an interger value and a left child node and a right child node, A node is said to be a valid BST node if and only if it satisfies the BST property:
+//its value is strictly greater than the values of every node to its left; its value is less than or equal to every node its right; and its childred nodes are either BST nodes
+// or None/null
+
+
+//Write BST class
